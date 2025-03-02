0e245589a7e44883c4eb5e24802de553fae0b999
黃鉅燊 <matchaskill@gmail.com>
2025-03-03 04:17:39 +0800

    新增字體和圖示資源
    - 在 nuxt.config.ts 中添加 Font Awesome 圖示庫的 CDN 連結
    - 引入 Montserrat 字體，支持多種字重
    - 優化網站的排版和圖示顯示

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,6 +45,14 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/Personal_Web/images/favicon.png' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/Personal_Web/images/favicon.png' },
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap',
+        },
       ],
     },
   },
